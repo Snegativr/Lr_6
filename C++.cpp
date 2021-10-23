6d7e40a291edbc82c6457efc4b5f5ff4c8cbb330
Snegativr <93019177+Snegativr@users.noreply.github.com>
2021-10-23 05:30:06 +0300

    Add files via upload

---
+++ C++.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+using namespace std;
+int main()
+{
+	float  y, min, max, step;
+	cout << "min=";
+	cin >> min;
+	cout << "max=";
+	cin >> max;
+	cout << "step=";
+	cin >> step;
+	while (min <= max) {
+				y = pow(sin(min), 2) + 2 * cos(min) + 3;
+		cout <<setw(10)<<"y=" << y << setw(10) <<"x="<< min << "\n";
+		min += step;
+
+	}
+}
